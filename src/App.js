1ea5b7522f6e9f54266e361573a3e0a271d753c0
Nick McCurdy <nick@nickmccurdy.com>
2019-05-18 22:01:31 -0400

    Render balance with hooks

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-/* global  ethereum:false, Web3:false */
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+/* global ethereum:false, Web3:false */
+import { useEffect, useState } from "react";
 
-const web3 = new Web3(ethereum);
+export default function App() {
+  const [balance, setBalance] = useState();
 
-web3.eth.getAccounts((error, [account]) => {
-  web3.eth.getBalance(account, (error, balanceWei) => {
-    const balance = web3.fromWei(balanceWei.toNumber(), "ether");
-    console.log(balance);
+  useEffect(() => {
+    const web3 = new Web3(ethereum);
+
+    web3.eth.getAccounts((error, [account]) => {
+      web3.eth.getBalance(account, (error, balanceWei) => {
+        setBalance(web3.fromWei(balanceWei.toNumber(), "ether"));
+      });
+    });
   });
-});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return balance || "Loading...";
 }
-
-export default App;
