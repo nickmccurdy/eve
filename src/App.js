d5dbd1b6dd36b00aa9f817fbf8524d466e7dd51e
Nick McCurdy <nick@nickmccurdy.com>
2019-05-18 21:46:30 -0400

    Log current balance with MetaMask

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* global  ethereum:false, Web3:false */
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const web3 = new Web3(ethereum);
+
+web3.eth.getAccounts((error, [account]) => {
+  web3.eth.getBalance(account, (error, balanceWei) => {
+    const balance = web3.fromWei(balanceWei.toNumber(), "ether");
+    console.log(balance);
+  });
+});
 
 function App() {
   return (
