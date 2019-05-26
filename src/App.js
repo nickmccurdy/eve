38cd0b60b1414379692d5c55c4bad5abb2d8b1e4
Nick McCurdy <nick@nickmccurdy.com>
2019-05-25 21:12:28 -0400

    Use ethereum.enable() to support EIP-1102

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ export default function App() {
   const [balance, setBalance] = useState();
 
   useEffect(() => {
-    const web3 = new Web3(ethereum);
+    async function fetchBalance() {
+      const web3 = new Web3(ethereum);
 
-    web3.eth.getAccounts((error, [account]) => {
-      web3.eth.getBalance(account, (error, balanceWei) => {
-        setBalance(web3.fromWei(balanceWei.toNumber(), "ether"));
+      await ethereum.enable();
+
+      web3.eth.getAccounts((error, [account]) => {
+        web3.eth.getBalance(account, (error, balanceWei) => {
+          setBalance(web3.fromWei(balanceWei.toNumber(), "ether"));
+        });
       });
-    });
+    }
+
+    fetchBalance();
   });
 
   return balance || "Loading...";
