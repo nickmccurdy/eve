82983fdf642d2629ab25a0a54192e86bf8110ce6
Nick McCurdy <nick@nickmccurdy.com>
2019-05-27 10:28:26 -0400

    Run both requests in parallel

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,21 @@ import ENS from "ethereum-ens";
 import React, { useEffect, useState } from "react";
 import Web3 from "web3";
 
+const web3 = new Web3(ethereum);
+const ens = new ENS(ethereum);
+
 const App: React.FC = () => {
   const [state, setState] = useState({ balance: "", name: "" });
 
   useEffect(() => {
     async function fetchBalance() {
       try {
-        const web3 = new Web3(ethereum);
         const [account] = await ethereum.enable();
-        const balance = web3.utils.fromWei(await web3.eth.getBalance(account));
-
-        const ens = new ENS(ethereum);
-        const name = await ens.reverse(account).name();
-
-        setState({ balance, name });
+        const [balanceWei, name] = await Promise.all([
+          web3.eth.getBalance(account),
+          ens.reverse(account).name()
+        ]);
+        setState({ balance: web3.utils.fromWei(balanceWei), name });
       } catch (error) {
         console.error(error);
       }
