d5f69f99670f8f1bf03c86856b345f1aafaf75a3
Nick McCurdy <nick@nickmccurdy.com>
2019-05-27 10:06:43 -0400

    Reverse resolve ENS name

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 /* global ethereum:false */
+import ENS from "ethereum-ens";
 import React, { useEffect, useState } from "react";
 import Web3 from "web3";
 
 const App: React.FC = () => {
   const [balance, setBalance] = useState("");
+  const [name, setName] = useState("");
 
   useEffect(() => {
     async function fetchBalance() {
@@ -11,6 +13,9 @@ const App: React.FC = () => {
         const web3 = new Web3(ethereum);
         const [account] = await ethereum.enable();
         setBalance(web3.utils.fromWei(await web3.eth.getBalance(account)));
+
+        const ens = new ENS(ethereum);
+        setName(await ens.reverse(account).name());
       } catch (error) {
         console.error(error);
       }
@@ -19,7 +24,13 @@ const App: React.FC = () => {
     fetchBalance();
   });
 
-  return <>{balance || "Loading..."}</>;
+  return name && balance ? (
+    <>
+      {name}: {balance}
+    </>
+  ) : (
+    <>Loading...</>
+  );
 };
 
 export default App;
